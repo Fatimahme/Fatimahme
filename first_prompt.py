a52fc4bf8ed22bd7a09fbdc053dfed08e99a2515
Fatimahme <153767462+Fatimahme@users.noreply.github.com>
2025-02-20 20:39:30 +0330

    Update first_prompt.py

--- first_prompt.py
+++ first_prompt.py
@@ -1,26 +1,12 @@
 from langchain_openai import ChatOpenAI  # pip install -U langchain_openai
 
-# messages = [
-#     {"role": "system", "content": "You are a helpful assistant."},
-#     {"role": "user", "content": "Hello world!"},
-# ]
-# # استفاده از مدل gpt-4o-mini
-# model_name = "gpt-4o-mini"
-
-
-# llm = ChatOpenAI(
-#     model=model_name, base_url="https://api.avalai.ir/v1", api_key="aa-Z0DGD0GsA8Y59MHb92ZUKfSPwWRpBUrfObPOX8t83VeYX0vA"
-# )
-
-# llm.invoke(messages)
-
 llm = ChatOpenAI(
     model="gpt-4o-mini",
     base_url="https://api.avalai.ir/v1",
     api_key="aa-Z0DGD0GsA8Y59MHb92ZUKfSPwWRpBUrfObPOX8t83VeYX0vA"  # Replace with your actual API key
 )
 
-# Get topic input from the user
+# Get topic input from the user for examlpe music or astronomy or llm or blah blah blah :)))
 topic = input("Enter a topic for the article: ")
 
 # Create the prompt for OpenAI
